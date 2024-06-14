246b1a4d418479e9f76784bdca4709a3302152a8
Yuri69420 <146597745+Yuri69420@users.noreply.github.com>
2024-06-14 04:32:18 +0200

    UwwwU

--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function() {
             message = 'Please enter a valid phone number.';
         } else {
             document.getElementById('formMessage').textContent = 'Submitting...';
-            fetch('/register', {
+            fetch('/src/pages/thankyou.html', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
